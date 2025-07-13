b005e688ad0a2e4a4e37a6bd74e3efa4c121a091
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-07-14 00:44:38 +1200

    Fix static asset paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
+  assetPrefix: '',
 }
 
-module.exports = nextConfig
+export default nextConfig
