7439b3e2cc214b7c31bb73a5c6d0e7675ae94746
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-07-14 02:59:29 +1200

    make project section responsive

--- src/components/projects/ProjectsBody.tsx
+++ src/components/projects/ProjectsBody.tsx
@@ -7,7 +7,7 @@ import Meow from './Meow'
 
 export default function ProjectBody() {
   return (
-    <div className="text-black bg-white rounded-lg shadow-lg my-3 w-fit mx-auto flex sm:max-w-7xl flex-col items-center text-center">
+    <div className="text-black bg-white rounded-lg shadow-lg my-3 w-fit mx-auto flex  max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-[90vw] flex-col items-center text-center">
       <div className="">
         <h1 className="text-3xl font-bold mt-6 mx-4 text-black bg-green-200 py-2 font-mono px-3 inline-block">
           Full-Stack Projects
