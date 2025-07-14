230d31e04e78e353a7e00507dbb6f89cd22db738
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-07-14 12:19:18 +1200

    implement animation to about section header, subheader and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import Image from 'next/image'
-import { Download, Send } from 'lucide-react'
 import MarqLandF from '@/components/skills/MarqLandF'
 import MarqBackend from '@/components/skills/MarqBackend'
 import MarqStyling from '@/components/skills/MarqStyling'
@@ -10,6 +8,7 @@ import Icons from '@/components/skills/Icons'
 import Contact from '@/components/Contact'
 import Navbar from '@/components/Navbar'
 import ProjectBody from '@/components/projects/ProjectsBody'
+import AboutMe from '@/components/AboutMe'
 
 export default function Home() {
   return (
@@ -20,65 +19,7 @@ export default function Home() {
         id="about"
         className="min-h-screen p-8 pt-24 scroll-mt-24 flex flex-col items-center justify-center text-center snap-start h-screen"
       >
-        <div className="flex flex-col md:flex-row gap-10 items-center max-w-7xl w-full">
-          <div className="p-8 rounded-lg shadow-lg max-w-3xl w-full bg-white">
-            <h2 className="text-5xl font-black text-black bg-green-200 py-2 font-mono max-w-fit px-3">
-              EUGENE
-            </h2>
-            <h3 className="text-4xl font-black mb-4 text-black py-2 font-mono max-w-fit">
-              Full-Stack Developer
-            </h3>
-            <p className="max-w-3xl leading-relaxed text-left text-black text-md">
-              Hey! 👋 I build full-stack web applications that combine clean
-              interfaces with robust, scalable backend systems and smart AI
-              integration. From crafting intuitive frontends to architecting
-              efficient APIs and databases, I love creating seamless, end-to-end
-              experiences that make complex tech feel simple and human-centered.
-              <br />
-              <br />
-              My drive for tech reflects my passion for technical rock climbing,
-              canyoning and caving — for the learning, teaching, problem-solving
-              and team work involved. Like getting people outdoors,{' '}
-              <span className="font-semibold">
-                I get excited from people using the applications I&#39;ve built
-              </span>
-              .
-              <br />
-              <br />
-              With digital marketing and communications experience from another
-              life, I bring a creative mindset, strong human skills, fast
-              learning, and sharp attention to detail to project collaborations.
-            </p>
-            <div className="mt-6 flex flex-col sm:flex-row gap-4">
-              <a
-                href="/Eugene Yeo_CV.pdf"
-                download
-                className=" bg-black text-white font-bold px-4 py-2 rounded hover:bg-green-200 hover:text-black hover:font-bold transition flex items-center gap-2"
-              >
-                <Download size={20} />
-                Download CV
-              </a>
-
-              <button
-                onClick={() => {
-                  const el = document.getElementById('contact')
-                  el?.scrollIntoView({ behavior: 'smooth' })
-                }}
-                className="border text-black font-bold px-4 py-2 rounded hover:bg-green-200  hover:border-white transition flex items-center gap-2"
-              >
-                <Send size={20} />
-                Contact Me
-              </button>
-            </div>
-          </div>
-          <Image
-            src="/images/DSC04088.avif"
-            alt="Profile picture"
-            width={100}
-            height={100}
-            className="min-w-md object-cover border-green-300 border-10 "
-          />
-        </div>
+        <AboutMe />
       </section>
 
       {/* Skills Section */}
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,17 +9,22 @@ export default function AboutMe() {
     <div className="flex flex-col md:flex-row gap-10 items-center max-w-7xl w-full">
       <div className="p-8 rounded-lg shadow-lg max-w-3xl w-full bg-white">
         <motion.h2
-          initial={{ x: -100, opacity: 0 }}
+          initial={{ x: -400, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
           className="text-5xl font-black text-black bg-green-200 py-2 font-mono max-w-fit px-3"
         >
           EUGENE
         </motion.h2>
 
-        <h3 className="text-4xl font-black mb-4 text-black py-2 font-mono max-w-fit">
+        <motion.h3
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1, duration: 1, ease: 'easeOut' }}
+          className="text-4xl font-black mb-4 text-black py-2 font-mono max-w-fit"
+        >
           Full-Stack Developer
-        </h3>
+        </motion.h3>
         <p className="max-w-3xl leading-relaxed text-left text-black text-md">
           Hey! 👋 I build full-stack web applications that combine clean
           interfaces with robust, scalable backend systems and smart AI
@@ -41,7 +46,14 @@ export default function AboutMe() {
           life, I bring a creative mindset, strong human skills, fast learning,
           and sharp attention to detail to project collaborations.
         </p>
-        <div className="mt-6 flex flex-col sm:flex-row gap-4">
+
+        {/* buttons */}
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 2, duration: 1, ease: 'easeOut' }}
+          className="mt-6 flex flex-col sm:flex-row gap-4"
+        >
           <a
             href="/Eugene Yeo_CV.pdf"
             download
@@ -61,8 +73,10 @@ export default function AboutMe() {
             <Send size={20} />
             Contact Me
           </button>
-        </div>
+        </motion.div>
       </div>
+
+      {/* Profile Image */}
       <Image
         src="/images/DSC04088.avif"
         alt="Profile picture"
